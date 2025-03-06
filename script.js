31bda95b5ae473e7ca6e91baebbff77c12c1a96e
afstudiox <afelipes@gmail.com>
2025-03-06 15:25:59 -0300

    Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const criarTarefaButton = document.getElementById('criar-tarefa');
 const textoTarefaInput = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
+
 // Adiciona o evento de clique no botão
 criarTarefaButton.addEventListener('click', function() {
   // Verifica se o campo de input não está vazio
@@ -12,9 +13,13 @@ criarTarefaButton.addEventListener('click', function() {
     // Cria um novo item de lista <li>
     const novaTarefa = document.createElement('li');
     novaTarefa.textContent = textoTarefa;
+    novaTarefa.addEventListener('click', function(){
+      novaTarefa.classList.toggle('bg-color-grey');
+    })
     
     // Adiciona o novo item ao final da lista
     listaTarefas.appendChild(novaTarefa);
+
     
     // Limpa o campo de input
     textoTarefaInput.value = "";
